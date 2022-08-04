69613dfa4a50643941fdd3b2171bd421642ed463
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-05 10:37:20 +1200

    Tour model

--- Api/models.py
+++ Api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tour(models.Model):
+    name = models.CharField(max_length=55, blank=False)
+    description = models.TextField(max_length=300, blank=False)
+
+
+    
+
+
+
+
