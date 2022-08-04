a98ce40db5b0b67128a1e2ad91c70603139250f3
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-05 11:07:29 +1200

    Admin Model register and format dashboard

--- Api/models.py
+++ Api/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Tour(models.Model):
     name = models.CharField(max_length=55, blank=False)
     description = models.TextField(max_length=300, blank=False)
-    
+
+    def __str__(self):
+        return self.name
 
 
 
