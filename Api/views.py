5664747f8e2dc60e41b910a1f6fe2f1c0e811fc6
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-05 10:53:27 +1200

    Views and Queryset

--- Api/views.py
+++ Api/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from .models import Tour
+from .serializers import TourSerializer, UserSerializer
+#from rest_framework.authentication import TokenAuthentication
+#from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TourViewSet(viewsets.ModelViewSet):
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializer
+
+
+
 
 
 
