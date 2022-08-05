9545c200f54d96ee50e604c7aa4b52e45d14c90a
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-05 15:20:34 +1200

    Admin Options

--- Api/admin.py
+++ Api/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from .models import Tour, Agent
 
-admin.site.register(Tour)
-admin.site.register(Agent)
+#admin.site.register(Tour)
+#admin.site.register(Agent)
 
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    list_display = ['name', 'description']
+    list_filter= ['name']
+    search_fields = ['name']
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name']
+    list_display = ['first_name', 'last_name']
+    #list_filter= ['genre']
+    search_fields = ['first_name']
 
 
 
