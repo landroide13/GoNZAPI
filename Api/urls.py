b283c0ef6043681dc7ac3817cbcf54521d14ba64
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-05 12:30:41 +1200

    correction

--- Api/urls.py
+++ Api/urls.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 
-from Api.views import TourViewSet, UserViewSet
+from Api.views import TourViewSet, UserViewSet, AgentViewSet
 from django.conf.urls import include
 
 router = routers.DefaultRouter()
 router.register('tours', TourViewSet)
 router.register('users', UserViewSet)
-router.register('agents', UserViewSet)
+router.register('agents', AgentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
